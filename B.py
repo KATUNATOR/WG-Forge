b5ad8c560b3d72cfdc487cf4d3261fcc467b4bb2
Kate Hinkel <37249744+KATUNATOR@users.noreply.github.com>
2018-09-16 18:40:36 +0300

    Add files via upload

---
+++ B.py
@@ -0,0 +1,14 @@
+s = int(input())
+x = int(input())
+y = int(input())
+vb = int(input())
+vp = int(input())
+p = int(input())
+db = int(input())
+
+sp = abs(x - y);
+if y > x:
+	dp = 1
+else:
+	dp = -1
+
