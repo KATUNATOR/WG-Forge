ef584f46d392a5b19e2c19df698843ffd32610d0
Kate Hinkel <hinckel.katia@gmail.com>
2018-09-17 20:52:44 +0300

    Bruteforce

--- C.py
+++ C.py
@@ -1,12 +1,12 @@
 n = int(raw_input())
 
-old = []
 names = []
 
 for i in range(n):
-    names.insert(0, raw_input())
+    name = raw_input()
+    if name in names:
+        names.remove(name)
+    names.insert(0, name)
 
 for i in names:
-    if not (i in old):
-        print(i)
-        old.append(i)
+    print(i)
