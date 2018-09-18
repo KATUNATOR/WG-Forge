f164c9042459916b9921249ffac43cefc0b36a14
Kate Hinkel <hinckel.katia@gmail.com>
2018-09-18 22:19:36 +0300

    I'm still durik

--- A.py
+++ A.py
@@ -8,17 +8,17 @@ for i in range(nd):
     days.append([2 * int(raw_input()), 0])
 
 count = 0
+prew = [-1,-1]
 for d in sorted(days):
-    if days.index(d) != 0 and d[0] == days[days.index(d) - 1][0]:
-        d[1] = count
-    else:
+    if d != prew:
         for m in meals[:]:
             if d[0] >= m:
                 count += 1
                 meals.remove(m)
             else:
                 break
-        d[1] = count
+    d[1] = count
+    prew = d
 
 for i in days:
     print(i[1])
